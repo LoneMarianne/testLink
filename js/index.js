d63526790c9784dba6a8a10ad1df00e493179b3b
LoneMarianne <loneoesterlund@gmail.com>
2018-01-11 11:21:31 +0100

    call by other function

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 //https://www.tutorialspoint.com/cordova/cordova_inappbrowser.htm
+function test(){
+	var url='https://cordova.apache.org';
+	var number = urlInput.value;
+	if(number == 1) url ='http://www.kea.dk';
+	if(number == 1) url ='https://www.dr.dk/';
+	openBrowser(url);
+}
 
-function openBrowser() {
-   var url = 'https://cordova.apache.org';
+function openBrowser(url) {
+   //var url = 'https://cordova.apache.org';
    var target = '_blank';
    var options = "location = yes"
    var ref = cordova.InAppBrowser.open(url, target, options);
