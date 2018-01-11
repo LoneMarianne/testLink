29fccad949439ef1a1e3a04058115e6216529174
LoneMarianne <loneoesterlund@gmail.com>
2018-01-11 11:24:13 +0100

    Finish

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function test(){
 	var url='https://cordova.apache.org';
 	var number = urlInput.value;
 	if(number == 1) url ='http://www.kea.dk';
-	if(number == 1) url ='https://www.dr.dk/';
+	if(number == 2) url ='https://www.dr.dk/';
 	openBrowser(url);
 }
 
